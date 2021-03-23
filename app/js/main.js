d7672bea76b25f8bb33938064dcaa4ae552f24d9
Valerii2390-dev <valezh14@yahoo.com>
2021-03-23 12:24:31 +0400

    added works services form

--- app/js/main.js
+++ app/js/main.js
@@ -9,4 +9,13 @@ $(function() {
         prevArrow: '<button class="slick-arrow arrow-prev"><img src="images/icons/arrow-left.svg" alt="prev arrow" /></button>',
         nextArrow: '<button class="slick-arrow arrow-next"><img src="images/icons/arrow-right.svg" alt="next arrow" /></button>',
     });
+
+    $('.service__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+        $('.service__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.service__tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return true;
+    });
 });
