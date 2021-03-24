bee0530cc6a4a1ce4e2f549ec5a0cdd6cbd31502
Valerii2390-dev <valezh14@yahoo.com>
2021-03-25 02:55:11 +0400

    added price

--- app/js/main.js
+++ app/js/main.js
@@ -19,6 +19,15 @@ $(function() {
         return true;
     });
 
+    $('.price__inner .tab1').on('click', function(event) {
+        var id = $(this).attr('data-id');
+        $('.price__inner').find('.tab-item1').removeClass('active-tab1').hide();
+        $('.price__inner').find('.tab1').removeClass('active1');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab1').fadeIn();
+        return true;
+    });
+
     $('.about__slider_inner').slick({
         autoplay: true,
         autoplaySpeed: 1000,
@@ -27,5 +36,22 @@ $(function() {
         slidesToShow: 1,
         slidesToScroll: 1,
         cssEase: 'linear',
+    });
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function(item) {
+                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+        }
     });
 });
